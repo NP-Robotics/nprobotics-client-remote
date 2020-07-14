00106aa175a7c25036548f160d8a74c0caac54a3
Yuzhe Jonathen Cheng <s10187979@connect.np.edu.sg>
2020-07-15 02:10:32 +0800

    chimes work in progess1.0

---
+++ src/models/meeting.js
@@ -0,0 +1,23 @@
+import { joinMeeting, endMeeting } from '../services/chime'
+
+export default {
+namespace: 'meeting',
+
+state: {
+  authKey: null,
+},
+
+subscriptions: {
+  /* setup({ dispatch, history }) {  // eslint-disable-line
+    history.listen(({ pathname }) => {
+      if (pathname === '/dashboard') {
+      }
+    });
+  }, */
+},
+}
+
+effects: {
+    
+
+}
