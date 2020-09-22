8b42690699653f67ecb422128fbafb54108f1e90
khayliang <khayliang@gmail.com>
2020-09-22 19:46:39 +0800

    resolve console.log slider state not displaying actual state

--- src/pages/robotPage.jsx
+++ src/pages/robotPage.jsx
@@ -34,8 +34,8 @@ const RobotPage = ({ user, dispatch, history }) => {
     messagebox: null,
     endpoint: null,
     locations: [],
-    linearSliderIntensity: 1,
-    angularSliderIntensity: 1,
+    linearSliderIntensity: 5,
+    angularSliderIntensity: 5,
     frequency: 200,
     interval: null,
   });
@@ -181,12 +181,12 @@ const RobotPage = ({ user, dispatch, history }) => {
 
   const handleLinearSliding = (value) => {
     setState({ ...state, linearSliderIntensity: value });
-    console.log(`Linear Velocity is at level: ${state.linearSliderIntensity}`);
+    console.log(`Linear Velocity is at level: ${value / 2}`);
   };
 
   const handleAngularSliding = (value) => {
     setState({ ...state, angularSliderIntensity: value });
-    console.log(`Angular Velocity is at level: ${state.angularSliderIntensity / 5}`);
+    console.log(`Angular Velocity is at level: ${value / 5}`);
   };
 
   const handleMouseUp = () => {
@@ -204,7 +204,7 @@ const RobotPage = ({ user, dispatch, history }) => {
       topic: '/cmd_vel',
       payload: {
         linear: {
-          x: state.linearSliderIntensity/2,
+          x: state.linearSliderIntensity / 2,
           y: 0,
           z: 0,
         },
@@ -429,11 +429,11 @@ const RobotPage = ({ user, dispatch, history }) => {
         </div>
         <Slider
           className={style.linearSlider}
-          min={0}
+          min={1}
           max={10}
           onChange={handleLinearSliding}
           range={false}
-          value={typeof state.linearSliderIntensity === 'number' ? state.linearSliderIntensity : 0}
+          value={state.linearSliderIntensity}
         />
       </div>
       <div>
@@ -449,13 +449,11 @@ const RobotPage = ({ user, dispatch, history }) => {
         </div>
         <Slider
           className={style.angularSlider}
-          min={0}
+          min={1}
           max={10}
           onChange={handleAngularSliding}
           range={false}
-          value={
-            typeof state.angularSliderIntensity === 'number' ? state.angularSliderIntensity : 0
-          }
+          value={state.angularSliderIntensity}
         />
       </div>
 
